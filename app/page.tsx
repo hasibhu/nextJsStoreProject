28d7620595f291ceba0822f29f35e8c4fbb34a37
hasibhu <hasibul1987@gmail.com>
2024-11-28 11:32:01 +0100

    Shadcn ui has been implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import Image from "next/image";
+
+
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
+
+
+
   return (
-   <h1>Homepage</h1>
+    <div>
+      <h1>Homepage</h1>
+      <Button variant="secondary" size="lg" className="capitalize m-8 ">
+        Click Me
+      </Button>
+   </div>
   );
 }
