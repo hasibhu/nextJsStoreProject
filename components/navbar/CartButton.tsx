f7cbbd898c308d8e03b1a15d85c142f9fd4ce1a9
hasibhu <hasibul1987@gmail.com>
2024-11-28 22:32:00 +0100

    har code cart has been designed

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import { LuShoppingCart } from 'react-icons/lu';
 
 const CartButton = () => {
+    const cartNumber = 10;
     return (
-        <div>
-            <h2>Cart</h2>
-        </div>
+        <Button asChild variant='outline' size='icon' className='flex justify-center items-center relative'>
+            <Link href={'/cart'}>
+                <LuShoppingCart className='text-4xl'></LuShoppingCart>
+                <span className='absolute -top-3 -right-2 text-white bg-primary font-bold rounded-full h-6 w-6 flex items-center justify-center '>{ cartNumber}</span>
+            </Link>
+        </Button>
     );
 };
 
